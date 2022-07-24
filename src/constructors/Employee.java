897ed153face8e2723b3463d79f8328dc0a2ad91
serdarfesli <saricizmelimehmetaga@gmail.com>
2022-07-24 19:59:45 +0300

    Miscorosoft Class

--- src/constructors/Employee.java
+++ src/constructors/Employee.java
@@ -1,27 +1,28 @@
 package constructors;
 
-public class EmployeeClass {
+public class Employee {
    public String name;
    public String jobTitle;
    public int ID;
    public double salary;
+   public static String companyName ="Microsoft";
 
-   public EmployeeClass() {
+   public Employee() {
       salary=10000;
    }
-   public EmployeeClass(String name) {
+   public Employee(String name) {
       this.name = name;
    }
-   public EmployeeClass(String name, String jobTitle) {
+   public Employee(String name, String jobTitle) {
       this.name = name;
       this.jobTitle = jobTitle;
    }
-   public EmployeeClass(String name, String jobTitle, int ID) {
+   public Employee(String name, String jobTitle, int ID) {
       this.name = name;
       this.jobTitle = jobTitle;
       this.ID = ID;
    }
-   public EmployeeClass(String name, String jobTitle, int ID, double salary) {
+   public Employee(String name, String jobTitle, int ID, double salary) {
       this.name = name;
       this.jobTitle = jobTitle;
       this.ID = ID;
@@ -34,6 +35,7 @@ public class EmployeeClass {
               ", jobTitle='" + jobTitle + '\'' +
               ", ID=" + ID +
               ", salary=" + salary +
+              " ,company Name=" + companyName +
               '}';
    }
 }
